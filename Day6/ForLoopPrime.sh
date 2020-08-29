309c272e9f7a563a08d9ffe987ac8417e755e81e
Arafat1999 <arafathonpode@gmail.com>
2020-08-29 16:05:06 +0530

    takes a input and determines if the number is prime

---
+++ Day6/ForLoopPrime.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -x
+
+#Write a program that takes a input and determines if the number is prime.
+
+echo "Enter Number"
+read num
+
+for((i=2; i<=num/2; i++))
+do
+  if [ $((num%i)) -eq 0 ]
+  then
+    echo "$num is not a prime number."
+    exit
+  fi
+done
+echo "$num is a prime number."
